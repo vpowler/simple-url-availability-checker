b18e4954444ed39f13f1ce527ac77ad54d66e1b4
val <vayne.powler@gmail.com>
2022-03-22 02:29:12 +0200

    Add timeout

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 export const config = Object.freeze({
+  urlTimeout: +process.env.TIMEOUT || 1000,
   urlsLine:
     process.env.URLS || "https://www.google.com https://www.amazon.com",
   sleepBetweenChecks: +process.env.SLEEP || 15,
-  alertInterval: +process.env.INTERVAL || 300,
+  alertInterval: +process.env.INTERVAL || 300
 });
--- src/package/util/network.ts
+++ src/package/util/network.ts
@@ -1,10 +1,11 @@
 import * as http from "http";
 import * as https from "https";
+import { config } from "../../config/config";
 
 export function request(url: string): Promise<{ code: number; text: string }> {
   const module = url.startsWith("https") ? https : http;
   return new Promise((resolve) => {
-    const req = module.request(url, { timeout: 1000, method: "GET" }, (res) => {
+    const req = module.request(url, { timeout: config.urlTimeout, method: "GET" }, (res) => {
       res.on("data", () => void 0);
       res.on("end", () =>
         resolve({ code: res.statusCode, text: res.statusMessage })
